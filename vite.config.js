ff1eb2595dc1cc9807f5cb2d8650f85f2b8770ea
lxry-comet <durdentagirowich@gmail.com>
2026-06-08 13:43:50 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
-// https://vite.dev/config/
 export default defineConfig({
-	base: '/harmony-of-nails/',
-	plugins: [react()]
-})
+	base: '/harmony-of-nails/', //! 👈 ВАЖЛИВО: вкажіть базу
+	root: 'src',
+	build: {
+		rollupOptions: {
+			//! ❌ Це шукає тільки HTML-файли верхнього рівня src/, і не включає HTML-файли нижнього рівня
+			// input: glob.sync('./src/*.html'),
+			//! ✅ Це шукає ВСІ HTML-файли, включаючи HTML-файли нижнього рівня
+			input: glob.sync('./src/**/*.html'),
+		},
+		outDir: '../dist',
+	},
+	plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+});
