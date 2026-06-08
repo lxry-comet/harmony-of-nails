7e8643e70c52b3b4223f524861e519da88d6bf3c
lxry-comet <durdentagirowich@gmail.com>
2026-06-08 13:49:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,17 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import { resolve } from 'path'; // 👈 Додаємо вбудований модуль path
 
 export default defineConfig({
-	base: '/harmony-of-nails/', //! 👈 ВАЖЛИВО: вкажіть базу
-	root: 'src',
-	build: {
-		rollupOptions: {
-			//! ❌ Це шукає тільки HTML-файли верхнього рівня src/, і не включає HTML-файли нижнього рівня
-			// input: glob.sync('./src/*.html'),
-			//! ✅ Це шукає ВСІ HTML-файли, включаючи HTML-файли нижнього рівня
-			input: glob.sync('./src/**/*.html'),
-		},
-		outDir: '../dist',
-	},
-	plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  base: '/harmony-of-nails/', 
+  root: 'src',
+  build: {
+    rollupOptions: {
+      // 🚀 Перетворюємо всі знайдені шляхи на абсолютні за допомогою resolve()
+      input: glob.sync('./src/**/*.html').map(file => resolve(file)),
+    },
+    outDir: '../dist', // Зберігаємо збірку на рівень вище папки src
+  },
+  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
